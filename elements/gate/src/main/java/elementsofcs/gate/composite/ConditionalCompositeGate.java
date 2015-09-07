15d79c861955ad1b6e45c6c57ab32b67ecd55b24
Brent Velthoen <brent.velthoen@gmail.com>
2015-09-07 16:21:55 -0700

    conditional composite gate

---
+++ elements/gate/src/main/java/elementsofcs/gate/composite/ConditionalCompositeGate.java
@@ -0,0 +1,37 @@
+package elementsofcs.gate.composite;
+
+import elementsofcs.gate.AbstractBinaryPredicateGate;
+import elementsofcs.gate.CompositeGate;
+import elementsofcs.gate.Pin;
+
+public class ConditionalCompositeGate extends AbstractBinaryPredicateGate implements CompositeGate {
+
+  private final OrCompositeGate or;
+  private final NotCompositeGate not;
+
+  private final Pin intermediate = new Pin("intermediate");
+
+  public ConditionalCompositeGate(Pin inputA, Pin inputB, Pin output) {
+    super(inputA, inputB, output);
+    not = new NotCompositeGate(inputA, intermediate);
+    or = new OrCompositeGate(intermediate, inputB, output);
+  }
+
+  @Override
+  public void eval() {
+    not.eval();
+    or.eval();
+  }
+
+  @Override
+  public void reset() {
+    not.reset();
+    or.reset();
+  }
+
+  @Override
+  public String toString() {
+    return "ConditionalCompositeGate [or=" + or + ", not=" + not + "]";
+  }
+
+}
